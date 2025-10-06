d5f45e0dafdfe9e233113a5e0fb46481972ce31d
niro667 <mekkyabonader@gmail.com>
2025-10-06 15:52:52 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,17 @@
-// hello.js
-console.log("Hello, world!");
+Expected = "Hello, Test!"
+OUTPUT=$ (node -e "console.log(require('./src/app')('Test'))")
+
+if ["$OUTPUT" == "$EXPECTED"];then
+
+echo = " test pass "
+
+exit 0
+
+else 
+
+echo = "a7a"
+
+exit 1
+
+fi
+
